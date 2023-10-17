d88f1b2c0e3ac83b18cd1e416abf44a334ce1bf0
Pahasara_112288 <pahasara.thisarakumara@axiatadigitallabs.com>
2023-10-17 18:06:25 +0530

    final

--- src/components/ReservationTable.js
+++ src/components/ReservationTable.js
@@ -1,150 +1,134 @@
 import React, { useEffect, useState } from 'react';
-import { Link ,useNavigate  } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import SweetAlert from 'react-bootstrap-sweetalert';
 import ReservationNavBar from './ReservationNavBar';
+import axios from 'axios'; // Import Axios
 
 const ReservationManage = () => {
-  // State to store reservations data
-  const [reservations, setReservations] = useState([]); 
-  // State to store train names mapping
+  const [reservations, setReservations] = useState([]);
   const [trainNames, setTrainNames] = useState({});
-  // State for error alert when cancelation condition is not met
   const [showErrorAlert, setShowErrorAlert] = useState(false);
-  // State for error alert when update condition is not met
   const [showErrorAlertUpdate, setShowErrorAlertUpdate] = useState(false);
 
-  // Initialize navigation
-  const navigate  = useNavigate (); // Initialize history
+  const navigate = useNavigate();
 
   useEffect(() => {
-    // Fetch reservations data from the API
-    fetch('http://localhost:5046/api/ticket/resv/all-reservations')
-      .then((response) => response.json())
-      .then((data) => setReservations(data.reservations || []))
-      .catch((error) => console.error('Error fetching data:', error));
+    // Fetch reservations data from the API using Axios
+    axios.get('http://localhost:5212/api/TicketReservation')
+      .then((response) => {
+        setReservations(response.data.reservations || []);
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
   }, []);
 
   useEffect(() => {
-    // Fetch available Train Names from the API
-    fetch('http://localhost:5046/api/train')
-      .then((response) => response.json())
-      .then((data) => {
+    // Fetch available Train Names from the API using Axios
+    axios.get('http://localhost:5212/api/traindetails')
+      .then((response) => {
         const trainNameMapping = {};
-        data.forEach((train) => {
+        response.data.forEach((train) => {
           trainNameMapping[train._id] = train.trainName;
         });
         setTrainNames(trainNameMapping);
       })
-      .catch((error) => console.error('Error fetching Train Names:', error));
+      .catch((error) => {
+        console.error('Error fetching Train Names:', error);
+      });
   }, []);
 
-  // Handle cancelation based on date difference
   const handleCancel = (reservationId, reservationDate) => {
-    // Calculate date difference logic here
     const currentDate = new Date();
     const cancelDate = new Date(reservationDate);
     const timeDifference = cancelDate.getTime() - currentDate.getTime();
     const daysDifference = Math.ceil(timeDifference / (1000 * 3600 * 24));
 
-    // Check if the condition for cancelation is met (e.g., 5 days before)
     if (daysDifference >= 5) {
       navigate(`/reservations/update/${reservationId}`);
     } else {
-      // If the condition is not met, show an error alert
       setShowErrorAlert(true);
     }
   };
 
-  // Handle updating based on date difference
   const handleUpdate = (reservationId, reservationDate) => {
-    // Calculate date difference logic here
     const currentDate = new Date();
     const cancelDate = new Date(reservationDate);
     const timeDifference = cancelDate.getTime() - currentDate.getTime();
     const daysDifference = Math.ceil(timeDifference / (1000 * 3600 * 24));
 
-    // Check if the condition for cancelation is met (e.g., 5 days before)
     if (daysDifference >= 5) {
       navigate(`/reservations/update/${reservationId}`);
     } else {
-      // If the condition is not met, show an error alert
       setShowErrorAlertUpdate(true);
     }
   };
 
   return (
     <>
-    <ReservationNavBar/>
-    <br></br>
-    <div>
-      <table className="table table-striped">
-        <thead>
-          <tr>
-            <th>Reference ID</th>
-            <th>Reservation Date</th>
-            <th>User ID</th>
-            <th>From</th>
-            <th>To</th>
-            <th>Status</th>
-            <th>Train Name</th>
-            <th>Update</th>
-            <th>Cancel</th>
-          </tr>
-        </thead>
-        <tbody>
-          {reservations.map((reservation) => {
-            return (
-              <tr key={reservation._id}>
-                <td>{reservation.referenceId}</td>
-                <td>{reservation.reservationDate.substring(0, 10)}</td>
-                <td>{reservation.userId}</td>
-                <td>{reservation.from}</td>
-                <td>{reservation.to}</td>
-                <td>{reservation.status ? 'true' : 'false'}</td>
-                <td>{trainNames[reservation.trainName]}</td>
-                <td className='text-center'>
-                 
-                    <button type="button" className="btn btn-secondary rounded-pill" onClick={()=>handleUpdate(reservation.id, reservation.reservationDate)}> 
-                        Update
+      <ReservationNavBar />
+      <br></br>
+      <div>
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Reference ID</th>
+              <th>Reservation Date</th>
+              <th>From</th>
+              <th>To</th>
+              <th>Update</th>
+              <th>Cancel</th>
+            </tr>
+          </thead>
+          <tbody>
+            {reservations.map((reservation) => {
+              return (
+                <tr key={reservation.id}>
+                  <td>{reservation.id}</td>
+                  <td>{reservation.referenceId}</td>
+                  <td>{reservation.reservationDate.substring(0, 10)}</td>
+                  <td>{reservation.fromDestination}</td>
+                  <td>{reservation.toDestination}</td>
+                  <td className='text-center'>
+                    <button type="button" className="btn btn-secondary rounded-pill" onClick={() => handleUpdate(reservation.id, reservation.reservationDate)}>
+                      Update
                     </button>
-           
-                </td>
-                <td className='text-center'>
-                  <button
-                    type="button"
-                    className="btn btn-danger rounded-pill"
-                    onClick={() => handleCancel(reservation.id, reservation.reservationDate)}
-                  >
-                    Cancel
-                  </button>
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-      
-      {/* Error Alert */}
-      {showErrorAlert && (
-        <SweetAlert
-          error
-          title="Error"
-          onConfirm={() => setShowErrorAlert(false)}
-        >
-          You cannot cancel this reservation because it's less than 5 days before the reservation date.
-        </SweetAlert>
-      )}
-        {/* Error Alert */}
+                  </td>
+                  <td className='text-center'>
+                    <button
+                      type="button"
+                      className="btn btn-danger rounded-pill"
+                      onClick={() => handleCancel(reservation.id, reservation.reservationDate)}
+                    >
+                      Cancel
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+        
+        {showErrorAlert && (
+          <SweetAlert
+            error
+            title="Error"
+            onConfirm={() => setShowErrorAlert(false)}
+          >
+            You cannot cancel this reservation because it's less than 5 days before the reservation date.
+          </SweetAlert>
+        )}
         {showErrorAlertUpdate && (
-        <SweetAlert
-          error
-          title="Error"
-          onConfirm={() => setShowErrorAlertUpdate(false)}
-        >
-          You cannot update this reservation because it's less than 5 days before the reservation date.
-        </SweetAlert>
-      )}
-    </div>
+          <SweetAlert
+            error
+            title="Error"
+            onConfirm={() => setShowErrorAlertUpdate(false)}
+          >
+            You cannot update this reservation because it's less than 5 days before the reservation date.
+          </SweetAlert>
+        )}
+      </div>
     </>
   );
 };
--- src/components/ScheduleTable.js
+++ src/components/ScheduleTable.js
@@ -1,173 +1,66 @@
 import React, { useEffect, useState } from 'react';
-// import '../../CSS/traintable.css';
 import { Link } from 'react-router-dom';
 import SweetAlert from 'react-bootstrap-sweetalert';
-import ScheduleNavBar from './ScheduleNavBar'
+import ScheduleNavBar from './ScheduleNavBar';
+import axios from 'axios';
 
 function ScheduleTable() {
-  // State variables for schedules, train names, route names, and the cancellation popup
   const [schedules, setSchedules] = useState([]);
-  const [trainNames, setTrainNames] = useState({});
-  const [routeNames, setRouteNames] = useState({});
-  const [stoppingStations, setStoppingStations] = useState({});
-  const [showCancelPopup, setShowCancelPopup] = useState(false); // State for SweetAlert popup
+  const [showCancelPopup, setShowCancelPopup] = useState(false);
 
   useEffect(() => {
-    // Fetch schedule data from the API
-    fetch('http://localhost:5046/api/TrainSchedule/')
-      .then((response) => response.json())
-      .then((data) => setSchedules(data))
-      .catch((error) => console.error('Error fetching data:', error));
-  }, []);
-
-  useEffect(() => {
-    // Fetch available Train IDs from the API
-    fetch('http://localhost:5046/api/train')
-      .then((response) => response.json())
-      .then((data) => {
-        const trainNameMapping = {};
-        data.forEach((train) => {
-          trainNameMapping[train.trainNo] = train.trainName;
-        });
-        setTrainNames(trainNameMapping);
+    // Fetch schedule data from the API using Axios
+    axios.get('http://localhost:5212/api/train-schedules/')
+      .then((response) => {
+        setSchedules(response.data);
       })
-      .catch((error) => console.error('Error fetching Train IDs:', error));
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
   }, []);
-
-  useEffect(() => {
-    // Fetch available Route data from the API
-    fetch('http://localhost:5046/api/route')
-      .then((response) => response.json())
-      .then((data) => {
-        const routeNamesMapping = {};
-        data.forEach((route) => {
-          routeNamesMapping[route.routeNo] = {
-            departure: route.departure,
-            arrival: route.arrival,
-          };
-        });
-        setRouteNames(routeNamesMapping);
-      })
-      .catch((error) => console.error('Error fetching Route data:', error));
-  }, []);
-
-  useEffect(() => {
-    // Fetch stopping station data for each schedule
-    schedules.forEach((schedule) => {
-      fetch(`http://localhost:5046/api/TrainSchedule/${schedule.id}/stopping-stations`)
-        .then((response) => response.json())
-        .then((data) => setStoppingStations((prevData) => ({ ...prevData, [schedule.id]: data })))
-        .catch((error) => console.error('Error fetching stopping stations:', error));
-    });
-  }, [schedules]);
 
   const handleDeleteSchedule = (id, reservationStatus) => {
     if (reservationStatus === 'Active') {
-      // Show a confirmation dialog if reservationStatus is Active
       setShowCancelPopup(true);
     } else {
       // Send a DELETE request to the API to delete the schedule by ID
-      fetch(`http://localhost:5046/api/TrainSchedule/delete-schedule/${id}`, {
-        method: 'DELETE',
-      })
+      axios.delete(`http://localhost:5046/api/train-schedules/${id}`)
         .then((response) => {
           if (response.status === 200) {
-            // Schedule successfully deleted, remove it from the state
             setSchedules((prevSchedules) => prevSchedules.filter((schedule) => schedule.id !== id));
           } else {
             console.error('Error deleting schedule:', response.statusText);
           }
         })
-        .catch((error) => console.error('Error deleting schedule:', error));
+        .catch((error) => {
+          console.error('Error deleting schedule:', error);
+        });
     }
   };
 
   return (
     <div className="container">
-      <ScheduleNavBar/>
+      <ScheduleNavBar />
       <br></br>
       <table className="table table-striped">
         <thead className="table header">
           <tr>
-            <th>Sche No</th>
-            <th>Arrival Date-Time</th>
-            <th>Departure Date-Time</th>
+            <th>ID</th>
             <th>Train Name</th>
-            <th>Route Details</th>
-            <th>Train Stopping Stations Departure & Arrival Times</th>
-            <th>Train Class Prices</th>
-            <th>Resev Status</th>
+            <th>Departure Time</th>
+            <th>From</th>
+            <th>Destination</th>
             <th>Schedule Actions</th>
           </tr>
         </thead>
         <tbody className="small">
           {schedules.map((schedule) => (
             <tr key={schedule.id}>
-              <td>{schedule.scheduleNo}</td>
-              <td className="col-md-7">
-                {schedule.arrivalDate.substring(0, 10)} - {schedule.arrivalTime}
-              </td>
-              <td className="col-md-3">
-                {schedule.departureDate.substring(0, 10)} - {schedule.departureTime}
-              </td>
-              <td className="col-md-3">{trainNames[schedule.trainNo]}</td>
-              <td className="col-md-3">
-                {routeNames[schedule.routeNo]?.departure} - {routeNames[schedule.routeNo]?.arrival}
-              </td>
-              <td>
-                <table style={{ width: '200px' }}>
-                  <thead>
-                    <tr>
-                      <th style={{ backgroundColor: '#7490df' }}>Station</th>
-                      <th style={{ backgroundColor: '#7490df' }}>D:Time</th>
-                      <th style={{ backgroundColor: '#7490df' }}>A:Time</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {stoppingStations[schedule.id]?.map((station) => (
-                      <tr key={station.id}>
-                        <td>{station.stationName}</td>
-                        <td>{station.arrivalTime.substring(0, 5)}</td>
-                        <td>{station.departureTime.substring(0, 5)}</td>
-                      </tr>
-                    ))}
-                  </tbody>
-                </table>
-              </td>
-              <td>
-                <table>
-                  <tbody>
-                    <tr>
-                      <th style={{ backgroundColor: '#7490df' }}>Class</th>
-                      <th style={{ backgroundColor: '#7490df' }}>Price</th>
-                    </tr>
-                    <tr>
-                      <td>FC</td>
-                      <td>{schedule.firstClassPrice}</td>
-                    </tr>
-                    <tr>
-                      <td>SC</td>
-                      <td>{schedule.secondClassPrice}</td>
-                    </tr>
-                    <tr>
-                      <td>TC</td>
-                      <td>{schedule.thirdClassPrice}</td>
-                    </tr>
-                  </tbody>
-                </table>
-              </td>
-              <td>
-                {schedule.reservationStatus === 'Active' ? (
-                  <div className="container">
-                    <span className="active-indicator"></span>
-                  </div>
-                ) : (
-                  <div className="container">
-                    <span className="inactive-indicator"></span>
-                  </div>
-                )}
-                {schedule.reservationStatus === 'Active' ? 'Active' : 'Inactive'}
-              </td>
+              <td>{schedule.id}</td>
+              <td>{schedule.trainname}</td>
+              <td>{schedule.departuretime}</td>
+              <td>{schedule.from}</td>
+              <td>{schedule.destination}</td>
               <td className="text-center">
                 <Link to={`/schedules/update/${schedule.id}`}>
                   <button type="button" className="btn btn-success btn-md mb-2 rounded-pill small">
@@ -194,7 +87,6 @@ function ScheduleTable() {
           title="Cannot Cancel Train"
           onConfirm={() => {
             setShowCancelPopup(false);
-            // Handle any other logic here
           }}
           onCancel={() => setShowCancelPopup(false)}
           focusCancelBtn

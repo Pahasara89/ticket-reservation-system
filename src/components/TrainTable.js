f351c17131bf0de3f80744891b2d9db9c7aa7745
Pahasara_112288 <pahasara.thisarakumara@axiatadigitallabs.com>
2023-10-17 17:10:53 +0530

    traindetail

--- src/components/TrainTable.js
+++ src/components/TrainTable.js
@@ -1,51 +1,50 @@
 import React, { useEffect, useState } from 'react';
-// import '../../CSS/traintable.css'
 import { Link } from 'react-router-dom';
-import ViewTrain from './ViewTrain.css';
-import TrainNavBar from './TrainNavBar'
+import axios from 'axios'; // Import Axios
+
+import TrainNavBar from './TrainNavBar';
 
 function TrainTable() {
-  // State to store train data
   const [trains, setTrains] = useState([]);
 
   useEffect(() => {
-    // Fetch train data from your API when the component mounts
-    fetch('http://localhost:5046/api/train/') // Replace with your actual API endpoint
-      .then((response) => response.json())
-      .then((data) => setTrains(data))
-      .catch((error) => console.error('Error fetching data:', error));
+    // Use Axios to fetch train data
+    axios.get('http://localhost:5212/api/traindetails/') // Replace with your actual API endpoint
+      .then((response) => {
+        setTrains(response.data);
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
   }, []);
 
-  // Function to handle train deletion
   const handleDeleteSchedule = (id) => {
-    // Send a DELETE request to your API to delete the train by ID
-    fetch(`http://localhost:5046/api/train/delete-train/${id}`, {
-      method: 'DELETE',
-    })
+    // Use Axios to send a DELETE request
+    axios.delete(`http://localhost:5212/api/traindetails/${id}`)
       .then((response) => {
         if (response.status === 200) {
-          // Schedule successfully deleted, remove it from the state
           setTrains((prevTrains) => prevTrains.filter((train) => train.id !== id));
         } else {
           console.error('Error deleting train:', response.statusText);
         }
       })
-      .catch((error) => console.error('Error deleting train:', error));
+      .catch((error) => {
+        console.error('Error deleting train:', error);
+      });
   };
 
   return (
     <div className="containerssss">
-      <TrainNavBar/>
-      <br></br>
-      {/* <h1 className="my-4 text-center text-primary">Train Details</h1> */}
+      <TrainNavBar />
+      <br />
       <table className="table table-striped">
         <thead className='table header'>
           <tr>
-            <th>Train No</th> 
-            <th>Train Name</th> 
-            <th>First Class Capacity</th> 
-            <th>Second Class Capacity</th> 
-            <th>Third Class Capacity</th> 
+            <th>Train No</th>
+            <th>Train Name</th>
+            <th>First Class Capacity</th>
+            <th>Second Class Capacity</th>
+            <th>Third Class Capacity</th>
             <th>Type</th>
             <th>Published</th>
             <th>Active</th>
@@ -56,11 +55,11 @@ function TrainTable() {
         <tbody>
           {trains.map((train) => (
             <tr key={train.id}>
-              <td>{train.trainNo}</td> {/* Display Train No */}
+              <td>{train.trainNo}</td>
               <td>{train.trainName}</td>
-              <td>{train.firstClassCapacity}</td> 
+              <td>{train.firstClassCapacity}</td>
               <td>{train.secondClassCapacity}</td>
-              <td>{train.thirdClassCapacity}</td> 
+              <td>{train.thirdClassCapacity}</td>
               <td>{train.type}</td>
               <td>{train.isPublished ? 'Yes' : 'No'}</td>
               <td>{train.isActive ? 'Yes' : 'No'}</td>
@@ -72,10 +71,10 @@ function TrainTable() {
                 </Link>
               </td>
               <td className='text-center'>
-              <button
+                <button
                   type="button"
                   className="btn btn-danger mb-2 rounded-pill"
-                  onClick={() => handleDeleteSchedule(train.id)} // Attach the delete function
+                  onClick={() => handleDeleteSchedule(train.id)}
                 >
                   Delete
                 </button>
